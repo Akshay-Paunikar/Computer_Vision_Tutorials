c33859e854d516f70b40eb42ca9acabf30cc31e6
Akshay Paunikar <akshayakki29@gmail.com>
2024-03-08 23:16:55 +0530

    Draw a line on black screen

--- OpenCV_Python_Tutorials/Working_with_Images/Drawing_Functions/01_Draw_a_line.py
+++ OpenCV_Python_Tutorials/Working_with_Images/Drawing_Functions/01_Draw_a_line.py
@@ -1,8 +1,8 @@
-
+import numpy as np
 import cv2
 
 # read an input image
-image = cv2.imread("puppy.jpg")
+image = cv2.imread("puppy.jpg", 1)
 
 cv2.imshow("image", image)
 
@@ -10,6 +10,18 @@ cv2.imshow("image", image)
 image_line = cv2.line(image, (0,0), (250,250), (0,255,0), 9)
 
 cv2.imshow("image_line", image_line)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# draw a line on black screen using numpy library
+# create a black screen using numpy
+image = np.zeros((512, 512, 3), dtype='uint8')
+
+# draw a line
+image_black = cv2.line(image, (0,0), (250,250), (255, 255, 255), 9)
+
+cv2.imshow("image_black", image_black)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
