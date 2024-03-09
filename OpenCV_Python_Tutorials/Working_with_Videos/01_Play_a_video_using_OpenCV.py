a1b36270dd6143bb4b5ad478df56511b0f7d9a72
Akshay Paunikar <akshayakki29@gmail.com>
2024-03-10 00:08:21 +0530

    Play a video using OpenCV - input video file

--- OpenCV_Python_Tutorials/Working_with_Videos/01_Play_a_video_using_OpenCV.py
+++ OpenCV_Python_Tutorials/Working_with_Videos/01_Play_a_video_using_OpenCV.py
@@ -28,3 +28,26 @@ cap.release()
 cv2.destroyAllWindows()
 
 # create a video capture object and read the input file
+cap = cv2.VideoCapture("FranceTravel.mp4")
+
+# check if camera is opened or not
+if (cap.isOpened()==False):
+    print("Error in opening the camera")
+
+# read until video is completed
+while (cap.isOpened()):
+    ret, frame = cap.read() # capture frame by frame
+    if ret == True:
+        cv2.imshow("frame", frame) # display the resulting frame
+        # press "q" on keyboard to exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    # break the loop
+    else:
+        break
+
+# after everything is done release the video
+cap.release()
+
+# close all the frames
+cv2.destroyAllWindows()
